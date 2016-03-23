d11d6f2d5efd0e54872c02f362480941b6072dea
Roman Knura <roman.knura@dpdireland.ie>
2016-03-23 09:30:56 +0100

    Initial commit

---
+++ mavenTest01/src/main/java/ie/dpd/maventest01/HelloWorld.java
@@ -0,0 +1,15 @@
+package ie.dpd.maventest01;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author roman.knura
+ */
+public class HelloWorld {
+    
+}
